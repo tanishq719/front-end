d32a276ef52937133d63d5313430ccb329c62ec4
tanishq719 <tanishqshrivastava01@gmail.com>
2020-11-13 11:19:14 +0000

    merge resolved -g

--- q-n-q/src/components/Quest.vue
+++ q-n-q/src/components/Quest.vue
@@ -4,43 +4,45 @@
             <div>
                 <label>Title</label>
                 <input v-model="title" placeholder="">
-            <!-- <div class="body"> -->
-                <!-- <div> -->
-                <label>Body</label><br/>
-                <textarea v-model="body" rows=20 cols=80 placeholder="Explain">
-                </textarea>
-                <!-- </div> -->
-                
-            <!-- </div> -->
+            <div></div>
             </div>
-            <div style="display:flex; flex-direction:column-reverse;">
-                <ul>
-                    <li><img src="../assets/baseline_format_bold_black_18dp.png" width="25vh" height="25vh"></li>
-                    <li><img src="../assets/baseline_format_italic_black_18dp.png" width="25vh" height="25vh"></li>
-                    <li><img src="../assets/baseline_format_list_bulleted_black_18dp.png" width="25vh" height="25vh"></li>
-                    <li><img src="../assets/baseline_image_black_18dp.png" width="25vh" height="25vh"></li>
-                    <li><img src="../assets/baseline_link_black_18dp.png" width="25vh" height="25vh"></li>
-                    <li style="font-size:20px; padding:0 5px;">&#8721;</li>
-                </ul>
-            </div>
-            
+            <label>Body</label>
+            <TextEditor/>
+            <button style="margin: auto;" @click="postQuestion">Post</button>
         </div>
         <div class="select">
             <label>Choose an appropriate Group</label>
             <input placeholder="Global Group will be choosen by default">
             <label>Choose appropriate Tags</label>
             <input placeholder="Search">
-        </div>
+        </div>        
     </div>
 </template>
 
-<script>
+<script> 
+import { bus } from '../main'
+import TextEditor from './TextEditor'
+import { mapActions } from 'vuex'
 export default {
     name:"Quest",
+    components:{
+        TextEditor
+    },
     data: function(){
         return {
             title:"",
-            body:""
+            group:"",
+            tag:[]
+        }
+    },
+    methods:{
+        ...mapActions(['postThread']),
+        postQuestion: function()
+        {
+            bus.$emit('post-called');
+            this.$on('collect-body',(body)=>{
+                postThread(this.data.title,body,this.data.group,this.data.tag)
+            })
         }
     }
 }
@@ -48,33 +50,46 @@ export default {
 
 <style scoped>
 .quest{
-    width:100%;
-    margin: 2% 10%;
+    width:max-content;
+    margin: auto;
+    margin-top: 2%;
     display:flex;
     flex-direction: row;
-    justify-content: center;
-}
-ul{
-    list-style:none;
-    margin:0%;
-    padding:2vh 0vh;
+    justify-content:space-evenly;
 }
 .ask{
     display:flex;
+    flex-direction: column;
+    position: relative;
+    width:70%;
 }
-.body{
-    display: flex;
-    flex-direction: row;
-}
-input, textarea{
+input{
     display: block;
-    padding:5px;
-    margin:5px 0;
-    border-radius: 7px;
+    padding:3pt;
+    margin:3pt 0;
+    border-radius: 4pt;
     border-style: solid;
     border-color: #0a234d;
-    border-width: 2%;
+    border-width: 2pt;
     resize:none;
-    width:97%;
+    width: calc(100% - 35pt) 
+}
+.select{
+    width:25%;
+}
+button{
+ padding: 0px 10px 0px 10px;
+  background-color: #0a76cf;
+  color: white;
+  border-width: 2px;
+  border-radius: 5px;
+  border-color: #0a76cf;
+  width: 100%;
+  max-width: 120px;
+  border-style: solid;
+  text-align: center;
+  font-family: "Quicksand", sans-serif;
+  font-size: 14pt;
+  height:35pt;
 }
 </style>
---
+++ q-n-q/src/components/TextEditor.vue
@@ -0,0 +1,87 @@
+<template>
+    <div class="body">
+        <textarea v-bind:"content" rows=20 cols=80 placeholder="Explain">
+    {{ body }}
+        </textarea>
+        <div style="display:flex; flex-direction:column-reverse;">
+            <ul>
+                <li><img @click="styleLike($event)" name='bold' src="../assets/baseline_format_bold_black_18dp.png" width="25pt" height="25pt"></li>
+                <li><img @click="styleLike($event)" name='italic' src="../assets/baseline_format_italic_black_18dp.png" width="25pt" height="25pt"></li>
+                <li><img @click="styleLike($event)" name='list' src="../assets/baseline_format_list_bulleted_black_18dp.png" width="25pt" height="25pt"></li>
+                <li><img @click="styleLike($event)" name='image' src="../assets/baseline_image_black_18dp.png" width="25pt" height="25pt"></li>
+                <li><img @click="styleLike($event)" name='link' src="../assets/baseline_link_black_18dp.png" width="25pt" height="25pt"></li>
+                <li @click="styleLike($event)" name='math' style="font-size:20px; padding:0pt 8pt;">&#8721;</li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+import { bus } from '../main'
+export default {
+    name: "TextEditor",
+    data : function(){
+        return {
+            body:"Hello",
+            content:"",
+            bold: false,
+            italic: false,
+            list: false,
+            link: false
+        }
+    },
+    methods:{
+        styleLike: function(e){
+            if(e.target.name === 'bold'){
+                this.bold = !this.bold;
+            }
+        }
+    },
+    created(){
+        bus.$on('post-called',()=>this.$emit('collect-body',this.data.body))
+    },
+    watch:{
+        bold:function(){
+            if(this.bold)
+            {
+                
+            }
+        },
+        // body:function(){
+        //     if(this.bold){
+        //         var appendStr = this.body[this.body.length-1].bold()
+        //         this.body.slice(0,this.body.length-1);
+        //         this.body += appendStr;
+        //         console.log(this.body);
+        //     }
+                
+        // }
+    }
+}
+</script>
+
+<style>
+.body{
+    display: flex;
+    flex-direction: row;
+    position: relative;
+}
+ul{
+    list-style:none;
+    /* margin:2pt; */
+    padding:0;
+}
+li{
+    padding:0pt 3pt;
+}
+textarea{
+    padding:5px;
+    margin:5px 0;
+    border-radius: 7px;
+    border-style: solid;
+    border-color: #0a234d;
+    border-width: 2pt;
+    resize:none;
+    width:100%;
+}
+</style>
